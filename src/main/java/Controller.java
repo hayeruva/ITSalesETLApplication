fc6e85e16f1298f95d544cac498cc9fc7ea2d9d2
vn53wl0 <harshavardhan.reddy.yeruva@walmart.com>
2024-10-04 20:55:37 -0500

    API end points in controller

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,8 +1,8 @@
+import DTO.Employee;
 import DTO.ProfileMatch;
+import DTO.Recruiter;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,6 +12,46 @@ public class Controller {
     @GetMapping("/findMatch")
     public ResponseEntity findMatchingOpportunities(@RequestParam(required = true) List<String> skills, @RequestParam(required = true) List<String> locationPreferences, @RequestParam(required = true) List<String> positionType){
         return ResponseEntity.ok(ProfileMatch.class);
+    }
+
+    @GetMapping("/findEmployee")
+    public ResponseEntity findEmployee(@RequestParam(required = true) String name, @RequestParam(required = true) String email){
+        return ResponseEntity.ok(Employee.class);
+    }
+
+    @GetMapping("/findAllEmployees")
+    public ResponseEntity findAllEmployees(){
+        return ResponseEntity.ok(Employee.class);
+    }
+
+    @PostMapping("/saveEmployee")
+    public ResponseEntity saveEmployee(@RequestBody(required = true) Employee employee){
+        return ResponseEntity.ok(Employee.class);
+    }
+
+    @PostMapping("/saveEmployees")
+    public ResponseEntity saveEmployees(@RequestBody(required = true) List<Employee> employees){
+        return ResponseEntity.ok(Employee.class);
+    }
+
+    @GetMapping("/findRecruiter")
+    public ResponseEntity findRecruiter(@RequestParam(required = true) String name, @RequestParam(required = true) String email){
+        return ResponseEntity.ok(Recruiter.class);
+    }
+
+    @GetMapping("/findAllRecruiters")
+    public ResponseEntity findAllRecruiters(){
+        return ResponseEntity.ok(Recruiter.class);
+    }
+
+    @PostMapping("/saveRecruiter")
+    public ResponseEntity saveRecruiter(@RequestBody(required = true) Recruiter recruiter){
+        return ResponseEntity.ok(Recruiter.class);
+    }
+
+    @PostMapping("/saveRecruiters")
+    public ResponseEntity saveRecruiters(@RequestBody(required = true) List<Recruiter> recruiters){
+        return ResponseEntity.ok(Recruiter.class);
     }
 
 
